982811f48a58a6f63f851eb3b02105889ea08354
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-04-28 14:52:13 +0900

    공부

--- script/script.js
+++ script/script.js
@@ -1,7 +1,14 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    cardSectionSlide();
 })
 
-
+const cardSectionSlide = () => {
+    var swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+}

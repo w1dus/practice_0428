30a8e450fcba31f02f9b4309f4efabdd8ea0dc5c
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-05-08 17:33:30 +0900

    패럴렉스 테스트

--- script/script.js
+++ script/script.js
@@ -2,7 +2,101 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
     cardSectionSlide();
+    paralSlideSection();
 })
+
+
+const paralSlideSection = () => {
+    // 슬라이드
+    var swiper = new Swiper(".paralSlideSection .slideWrap .swiper", {
+        loop : true,
+        centeredSlides: true,
+        spaceBetween: 130,
+        navigation: {
+          nextEl: ".paralSlideSection .slideBox .pnBtnWrap .nextBtn",
+          prevEl: ".paralSlideSection .slideBox .pnBtnWrap .prevBtn",
+        },
+        breakpoints: {
+            0: {
+              slidesPerView: 1.5,
+              spaceBetween: 10,
+            },
+            650: {
+                slidesPerView: 1.5,
+                spaceBetween: 10,
+            },
+            950: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 1,
+              spaceBetween: 130,
+            },
+        },
+    });
+
+    // 패럴렉스
+    const slideWrap = document.querySelector('.paralSlideSection')
+
+    if (slideWrap) {
+
+        gsap.to(".paralSlideSection .hiddenWrap", {
+            width: "20vw",
+            height: "20vw",
+            maxWidth: "unset",
+            maxHeight: "unset",
+        });
+
+        // 스크롤에 따라 scrollBox를 고정 + 확대/축소 같은 애니메이션 하고 싶을 때
+        gsap.to(slideWrap, {
+            scrollTrigger: {
+                trigger: slideWrap,
+                start: "top 0%",  
+                end: "bottom top",  
+                pin: true,        
+                markers: false,
+            },
+        });
+
+        gsap.to(slideWrap,  
+            {
+            scrollTrigger: {
+                trigger: slideWrap,
+                start: "top 0%",   
+                end: "bottom top", 
+                markers: true,
+                onEnter: () => {
+                    gsap.to(".paralSlideSection .hiddenWrap", {
+                        duration: 1,  // 애니메이션 지속 시간
+                        width: "100%", // 애니메이션으로 width 변경
+                        height: "auto",   // 애니메이션으로 height 변경
+                        overflow: "hidden",
+                        ease: "power2.out", // 이징 효과 (선택 사항)
+                        onUpdate: () => {
+                            // 애니메이션 완료 후 overflow 제거
+                            swiper.update();
+                        }
+                    });
+
+                },
+                onLeaveBack: () => {
+                    gsap.to(".paralSlideSection .hiddenWrap", {
+                        // duration: 1,
+                        width: "20vw",  // 원래 크기
+                        height: "20vw", // 원래 크기
+                        // ease: "power2.in" // 이징 효과 (선택 사항)
+                    });
+                }
+            },
+        });
+    
+    } else {
+        return false;
+    }
+    
+
+}
 
 const cardSectionSlide = () => {
 
